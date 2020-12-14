c3c49982049606dd1ce22f1483cab051a7136786
Cheereus <fanwei1995@hotmail.com>
2020-12-14 13:40:55 +0800

    add python

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,21 +20,28 @@ module.exports = {
                 '2020-02-24',
             ],
             '/resume/': [
-                ''
+                '',
+                '2020-07-30'
             ],
             '/Java/': [
                 '',
                 '2020-05-21',
                 '2020-05-22',
                 '2020-05-24',
+            ],
+            '/Python/': [
+                '',
+                '2020-04-11',
+                '2020-04-18',
             ]
         },
         sidebarDepth: 5,
         nav: [
             { text: '首页', link: '/' },
             { text: '简历', link: '/resume/' },
-            { text: '统计学习方法', link: '/SLM/' },
+            { text: 'Python', link: '/Python/' },
             { text: 'Hadoop', link: '/Java/' },
+            { text: '统计学习方法', link: '/SLM/' },
             { text: 'Github', link: 'https://github.com/Cheereus' },
         ]
     },
